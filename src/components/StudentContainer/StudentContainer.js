9d2af9a912e9084e42ec84603a7ffdc3b00a534a
Victoria Kastanenka <vkastanenka@gmail.com>
2020-07-14 20:53:48 -0400

    can now filter by first and last name

--- src/components/StudentContainer/StudentContainer.js
+++ src/components/StudentContainer/StudentContainer.js
@@ -10,6 +10,9 @@ import useFetch from "../../utils/CustomHooks/useFetchData";
 
 const StudentContainer = () => {
   let content;
+  let studentArr;
+
+  const [filter, setFilter] = useState("");
 
   // Fetch students
   const res = useFetch("https://www.hatchways.io/api/assessment/students");
@@ -20,7 +23,7 @@ const StudentContainer = () => {
 
     // If no loading and successful response, display students
   } else if (!res.loading && res.res) {
-    const studentArr = res.res.data.students;
+    studentArr = res.res.data.students;
 
     // Add average and fullName fields to student object
     studentArr.forEach((student) => {
@@ -28,17 +31,24 @@ const StudentContainer = () => {
       student.fullName = `${student.firstName} ${student.lastName}`;
     });
 
-    content = studentArr.map((student) => (
-      <Student
-        key={student.id}
-        pic={student.pic}
-        fullName={student.fullName}
-        email={student.email}
-        company={student.company}
-        skill={student.skill}
-        average={student.average}
-      />
-    ));
+    // Render student component for filtered array
+    content = studentArr
+      .filter((student) => {
+        const lowerFilter = filter.toLowerCase();
+        const lowerName = student.fullName.toLowerCase();
+        return lowerName.includes(lowerFilter);
+      })
+      .map((student) => (
+        <Student
+          key={student.id}
+          pic={student.pic}
+          fullName={student.fullName}
+          email={student.email}
+          company={student.company}
+          skill={student.skill}
+          average={student.average}
+        />
+      ));
 
     // If no loading and error, display error message
   } else if (!res.loading && res.error) {
@@ -47,7 +57,14 @@ const StudentContainer = () => {
 
   return (
     <div className="student-container">
-      <input className="student-container__filter" type="text" />
+      <input
+        type="text"
+        value={filter}
+        id='name-input'
+        placeholder='Search by name'
+        className="input student-container__filter"
+        onChange={(e) => setFilter(e.target.value)}
+      />
       {content}
     </div>
   );
